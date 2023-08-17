43fcbd7323d32d9a36b582d7311ffd6e9a399173
jonatas1096 <jonatas1096@hotmail.com>
2023-08-17 00:36:39 -0300

    inserção do paulo roberto

--- app/src/main/java/com/example/projeto/views/Login.kt
+++ app/src/main/java/com/example/projeto/views/Login.kt
@@ -1,27 +1,19 @@
 package com.example.projeto.views
 
 import android.annotation.SuppressLint
-import android.content.Context
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.example.projeto.R
 import com.example.projeto.layoutsprontos.loadImage
 
 
@@ -36,12 +28,15 @@ fun Login() {
                 title = {
                     Text(text = "Sou aluno")
                 },
-                contentColor = Color.White
+                contentColor = Color.Black,
+                backgroundColor = Color.White
             )
         },
         modifier = Modifier.fillMaxSize(),
 
     ) {
+        
+        //Box para scalonar o background laranja
         Box(
             modifier = Modifier.fillMaxSize()
         ){
@@ -53,18 +48,47 @@ fun Login() {
 
             )
         }
+        //
 
+        
+        
         Column(
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+                .padding(horizontal = 45.dp)
+                .padding(bottom = 70.dp)
         ) {
-            loadImage(path = "",
-                contentDescription = "Capelo Aluno",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.size(10.dp)
-            )
+            //Já que eu estava tendo problemas com o tamanho da imagem, coloquei ela dentro de uma box e modifiquei o tamanho da box em si:
 
-        }
+            Box(
+                modifier = Modifier
+                    .size(140.dp)
+                    .padding(top = 30.dp)
+            ){
+
+                loadImage(path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/capelo.png",
+                    contentDescription = "Capelo Aluno",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                )
+            }
+
+            Spacer(modifier = Modifier.height(70.dp)) //isso é para dar um espaço entre as boxs
+
+            //Box branca que ficará os textfields:
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White,
+                        shape = RoundedCornerShape(15.dp)
+                    )
+
+            ){
+                Text(text = "A box está aqui")
+            }
+
+         }
 
 
 
