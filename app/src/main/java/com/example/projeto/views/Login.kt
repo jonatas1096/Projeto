769e6a9ce93c389787d2a62c6986b0c9a1bb425a
jonatas1096 <jonatas1096@hotmail.com>
2023-08-17 18:08:41 -0300

    Teste Box com constrainlayout

--- app/src/main/java/com/example/projeto/views/Login.kt
+++ app/src/main/java/com/example/projeto/views/Login.kt
@@ -2,10 +2,11 @@ package com.example.projeto.views
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,11 +16,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.projeto.layoutsprontos.loadImage
+import com.example.projeto.ui.theme.LARANJA
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun Login() {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+
     Box(
         modifier = Modifier.fillMaxSize()
     ){
@@ -37,7 +45,7 @@ fun Login() {
             modifier = Modifier.fillMaxSize()
 
         ) {
-            val (capeloBox,areaLogin) = createRefs()
+            val (capeloBox,areaLogin, botao) = createRefs()
 
             //Estava tendo problemas com o tamanho da imagem, então coloquei dentro de uma box e scalonei pela box:
             Box(
@@ -67,28 +75,62 @@ fun Login() {
                     }
                     .width(310.dp)
                     .height(430.dp)
-                    .background(Color.White)
+                    .background(
+                        Color.White,
+                        shape = RoundedCornerShape(30.dp)
+                    )
 
             ) {
                 Column(
                     modifier = Modifier
                         .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
+                    verticalArrangement = Arrangement.Top,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
 
+
                     Text(text = "Bem vindo!",
                         color = Color.Black,
-                        fontSize = 26.sp)
-
-                    Text(text = "Lorem!",
-                        color = Color(49, 48, 48, 255),
                         fontSize = 26.sp,
-                    modifier = Modifier.padding(top = 10.dp))
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .padding(bottom = 0.dp))
+
+                    Text(text = "Lorem ipsum lorem ipsu lorem!",
+                        color = Color(49, 48, 48, 255),
+                        fontSize = 16.sp,
+                        modifier = Modifier
+                            .padding(bottom = 40.dp)
+                    )
+
+
+                    //Email
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = {email = it},
+                        placeholder = {
+                                      Text(text = "Email")
+                        },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            unfocusedBorderColor = LARANJA
+                        ),
+                        modifier = Modifier.padding(horizontal = 40.dp)
+                    )
+
+
+                    //Logar
+                    Button(onClick = {
+
+                    },
+
+                    ) {
+
+                    }
                 }
 
             }
+
 
 
         }
