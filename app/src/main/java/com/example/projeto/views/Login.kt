392c09d157f4ca0530bcdaf18ff404ae582d76e1
jonatas1096 <jonatas1096@hotmail.com>
2023-08-17 16:26:58 -0300

    testando com constraint layout

--- app/src/main/java/com/example/projeto/views/Login.kt
+++ app/src/main/java/com/example/projeto/views/Login.kt
@@ -2,11 +2,9 @@ package com.example.projeto.views
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,85 +12,90 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.projeto.layoutsprontos.loadImage
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun Login() {
-
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Sou aluno")
-                },
-                contentColor = Color.Black,
-                backgroundColor = Color.White
-            )
-        },
-        modifier = Modifier.fillMaxSize(),
-
-    ) {
-        
-        //Box para scalonar o background laranja
-        Box(
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        loadImage(
+            path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/backgroundlaranja.png",
+            contentDescription = "Background Laranja",
+            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
-        ){
-            loadImage(
-                path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/backgroundlaranja.png",
-                contentDescription = "Background Laranja",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
 
-            )
-        }
-        //
+        )
+    }
 
-        
-        
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
+
+        ConstraintLayout(
             modifier = Modifier.fillMaxSize()
-                .padding(horizontal = 45.dp)
-                .padding(bottom = 70.dp)
+
         ) {
-            //Já que eu estava tendo problemas com o tamanho da imagem, coloquei ela dentro de uma box e modifiquei o tamanho da box em si:
+            val (capeloBox,areaLogin) = createRefs()
 
+            //Estava tendo problemas com o tamanho da imagem, então coloquei dentro de uma box e scalonei pela box:
             Box(
                 modifier = Modifier
-                    .size(140.dp)
-                    .padding(top = 30.dp)
-            ){
-
+                    .constrainAs(capeloBox) {
+                        top.linkTo(parent.top, margin = 50.dp)
+                        start.linkTo(parent.start, margin = 100.dp)
+                        end.linkTo(parent.end, margin = 100.dp)
+                    }
+                    .size(150.dp)
+            ) {
                 loadImage(path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/capelo.png",
-                    contentDescription = "Capelo Aluno",
+                    contentDescription = "Capelo de aluno",
                     contentScale = ContentScale.Fit,
                     modifier = Modifier
                 )
             }
 
-            Spacer(modifier = Modifier.height(70.dp)) //isso é para dar um espaço entre as boxs
-
-            //Box branca que ficará os textfields:
+            //Box que vai guardar email e senha:
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White,
-                        shape = RoundedCornerShape(15.dp)
-                    )
+                    .constrainAs(areaLogin) {
+                        top.linkTo(parent.top, margin = 130.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .width(310.dp)
+                    .height(430.dp)
+                    .background(Color.White)
 
-            ){
-                Text(text = "A box está aqui")
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+
+                    Text(text = "Bem vindo!",
+                        color = Color.Black,
+                        fontSize = 26.sp)
+
+                    Text(text = "Lorem!",
+                        color = Color(49, 48, 48, 255),
+                        fontSize = 26.sp,
+                    modifier = Modifier.padding(top = 10.dp))
+                }
+
             }
 
-         }
+
+        }
 
 
 
-    }
+
 }
 
 
