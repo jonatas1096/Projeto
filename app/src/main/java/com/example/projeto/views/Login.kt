4c6159c0c604e2d57bb496761f3dd08153da195b
jonatas1096 <jonatas1096@hotmail.com>
2023-08-18 18:11:24 -0300

    Bugs corrigidos e começo testes textfield

--- app/src/main/java/com/example/projeto/views/Login.kt
+++ app/src/main/java/com/example/projeto/views/Login.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -11,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -160,6 +163,7 @@ fun Login() {
                             colors = TextFieldDefaults.outlinedTextFieldColors(
                                 unfocusedBorderColor = LARANJA
                             ),
+                            maxLines = 1,
                             shape = RoundedCornerShape(40.dp),
                             modifier = Modifier
                                 .padding(horizontal = 40.dp)
@@ -180,11 +184,17 @@ fun Login() {
                             colors = TextFieldDefaults.outlinedTextFieldColors(
                                 unfocusedBorderColor = LARANJA
                             ),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            maxLines = 1,
+                            visualTransformation = PasswordVisualTransformation(),
                             shape = RoundedCornerShape(50.dp),
                             modifier = Modifier
                                 .padding(horizontal = 40.dp)
                                 .padding(bottom = 55.dp)
                         )
+
 
 
                         //Logar
