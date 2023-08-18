b7236bc835777810400ac2e225229321cc0c7961
jonatas1096 <jonatas1096@hotmail.com>
2023-08-18 17:02:59 -0300

    Gambiarra pra simular uma borda na box: concluída.

--- app/src/main/java/com/example/projeto/views/Login.kt
+++ app/src/main/java/com/example/projeto/views/Login.kt
@@ -4,13 +4,10 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -36,11 +33,13 @@ fun Login() {
     }
 
 
+
+    //Background ocupando toda a tela
     Box(
         modifier = Modifier.fillMaxSize()
     ){
         loadImage(
-            path = "",
+            path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/backgroundlogin.png",
             contentDescription = "Background do Login",
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
@@ -49,17 +48,19 @@ fun Login() {
     }
 
 
+
+
         ConstraintLayout(
             modifier = Modifier.fillMaxSize()
 
         ) {
-            val (capeloBox, areaLogin, titulo, pauloroberto) = createRefs()
+            val (capeloBox, areaLogin,areaLoginSOMBRA, titulo, pauloroberto) = createRefs()
 
             //Estava tendo problemas com o tamanho da imagem, então coloquei dentro de uma box e scalonei pela box:
             Box(
                 modifier = Modifier
                     .constrainAs(capeloBox) {
-                        top.linkTo(parent.top, margin = 40.dp)
+                        top.linkTo(parent.top, margin = 30.dp)
                         start.linkTo(parent.start, margin = 100.dp)
                         end.linkTo(parent.end, margin = 100.dp)
                     }
@@ -74,7 +75,7 @@ fun Login() {
 
             Text(text = "ConnectSTUDENT",
                 modifier = Modifier.constrainAs(titulo){
-                    top.linkTo(parent.top, margin = 120.dp)
+                    top.linkTo(parent.top, margin = 100.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 }
@@ -85,10 +86,12 @@ fun Login() {
                 )
 
 
-            //Box que vai guardar email e senha:
-            Box(
+        //Essa surface é uma gambiarra do carai só pra colocar uma sombra na Box abaixo, infelizmente o elevation padrão fica bugado:
+            Surface(
+                shape = RoundedCornerShape(30.dp),
+                elevation = 15.dp,
                 modifier = Modifier
-                    .constrainAs(areaLogin) {
+                    .constrainAs(areaLoginSOMBRA) {
                         top.linkTo(parent.top, margin = 130.dp)
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
@@ -96,109 +99,130 @@ fun Login() {
                     }
                     .width(310.dp)
                     .height(430.dp)
-                    .background(
-                        Color.White,
-                        shape = RoundedCornerShape(30.dp)
-                    )
+            ){}
 
 
+            //Box que vai guardar email e senha:
 
-
-
-            ) {
-                Column(
+                Box(
                     modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .constrainAs(areaLogin) {
+                            top.linkTo(parent.top, margin = 130.dp)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        }
+                        .width(310.dp)
+                        .height(430.dp)
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(30.dp)
+                        )
+
+
                 ) {
-
-
-
-                    Text(text = "Bem vindo!",
-                        color = Color.Black,
-                        fontSize = 26.sp,
-                    modifier = Modifier
-                        .padding(top = 10.dp)
-                        .padding(bottom = 0.dp))
-
-                    Text(text = "Lorem ipsum lorem ipsu lorem!",
-                        color = Color(49, 48, 48, 255),
-                        fontSize = 16.sp,
+                    Column(
                         modifier = Modifier
-                            .padding(bottom = 80.dp)
-                    )
-
-
-                    //Email
-                    OutlinedTextField(
-                        value = email,
-                        onValueChange = {email = it},
-                        placeholder = {
-                            Text(text = "Email",
-                                fontSize = 19.sp)
-                        },
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            unfocusedBorderColor = LARANJA
-                        ),
-                        shape = RoundedCornerShape(40.dp),
-                        modifier = Modifier
-                            .padding(horizontal = 40.dp)
-                            .padding(bottom = 20.dp)
-                    )
-
-
-                    //Senha
-                    OutlinedTextField(
-                        value = senha,
-                        onValueChange = { senha = it},
-                        placeholder = {
-                            Text(text = "Senha",
-                                fontSize = 19.sp)
-                        },
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            unfocusedBorderColor = LARANJA
-                        ),
-                        shape = RoundedCornerShape(50.dp),
-                        modifier = Modifier
-                            .padding(horizontal = 40.dp)
-                            .padding(bottom = 55.dp)
-                    )
-
-
-
-                    //Logar
-                    Button(onClick = {
-
-                    },
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = LARANJA,
-                            contentColor = Color.White,
-                        ),
-                        shape = RoundedCornerShape(30.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 90.dp)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(text = "Logar!",
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.Bold)
-                    }
-                }
 
-            }
+
+                        Text(
+                            text = "Bem vindo!",
+                            color = Color.Black,
+                            fontSize = 26.sp,
+                            modifier = Modifier
+                                .padding(top = 10.dp)
+                                .padding(bottom = 0.dp)
+                        )
+
+                        Text(
+                            text = "Lorem ipsum lorem ipsu lorem!",
+                            color = Color(49, 48, 48, 255),
+                            fontSize = 16.sp,
+                            modifier = Modifier
+                                .padding(bottom = 80.dp)
+                        )
+
+
+                        //Email
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            placeholder = {
+                                Text(
+                                    text = "Email",
+                                    fontSize = 19.sp
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                unfocusedBorderColor = LARANJA
+                            ),
+                            shape = RoundedCornerShape(40.dp),
+                            modifier = Modifier
+                                .padding(horizontal = 40.dp)
+                                .padding(bottom = 20.dp)
+                        )
+
+
+                        //Senha
+                        OutlinedTextField(
+                            value = senha,
+                            onValueChange = { senha = it },
+                            placeholder = {
+                                Text(
+                                    text = "Senha",
+                                    fontSize = 19.sp
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                unfocusedBorderColor = LARANJA
+                            ),
+                            shape = RoundedCornerShape(50.dp),
+                            modifier = Modifier
+                                .padding(horizontal = 40.dp)
+                                .padding(bottom = 55.dp)
+                        )
+
+
+                        //Logar
+                        Button(
+                            onClick = {
+
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = LARANJA,
+                                contentColor = Color.White,
+                            ),
+                            shape = RoundedCornerShape(30.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 90.dp)
+                        ) {
+                            Text(
+                                text = "Logar!",
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+
+                }
 
 
 
             //Paulo Roberto
             Box(
-                modifier = Modifier.constrainAs(pauloroberto){
-                    top.linkTo(parent.top, margin = 420.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end, margin = 270.dp)
-                    bottom.linkTo(parent.bottom)
+                modifier = Modifier
+                    .constrainAs(pauloroberto) {
+                        top.linkTo(parent.top, margin = 420.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end, margin = 270.dp)
+                        bottom.linkTo(parent.bottom)
 
-                }
+                    }
                     .size(140.dp)
             ) {
                 loadImage(path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/pauloroberto.png",
