6d4ea4a6f4c9f1045f367e499b9ceddbfec74978
jonatas1096 <jonatas1096@hotmail.com>
2023-08-30 23:22:44 -0300

    começo do layout e autenticação de professores/administração

--- app/src/main/java/com/example/projeto/repositorio/AuthRepositorio.kt
+++ app/src/main/java/com/example/projeto/repositorio/AuthRepositorio.kt
@@ -9,6 +9,7 @@ import javax.inject.Inject
 @ViewModelScoped
 class AuthRepositorio @Inject constructor(private val auth: Authentication) {
 
+    //Repositório dos dados dos alunos
     fun cadastro(nome:String, email: String, senha: String, rm:String, codigoturma:String, listenerAuth: ListenerAuth){
 
         auth.cadastroAluno(nome, email, senha,rm, codigoturma, listenerAuth)
@@ -16,5 +17,13 @@ class AuthRepositorio @Inject constructor(private val auth: Authentication) {
 
     fun login(email: String, senha: String, listenerAuth: ListenerAuth){
         auth.loginAluno(email,senha,listenerAuth)
+    }
+    ///////
+
+
+
+    //Repositório dos dados de professores/administração
+    fun cpsCadastro(nome:String, email: String, senha: String, id:String, codigoEtec:String, listenerAuth: ListenerAuth){
+        auth.cadastroCps(nome, email, senha, id, codigoEtec, listenerAuth)
     }
 }
--- app/src/main/java/com/example/projeto/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/projeto/viewmodel/AuthViewModel.kt
@@ -13,6 +13,7 @@ import javax.inject.Inject
 @HiltViewModel
 class AuthViewModel @Inject constructor(private val authRepositorio: AuthRepositorio): ViewModel() {
 
+    //View model dos alunos
     fun cadastro(nome:String, email: String, senha: String, rm:String, codigoturma:String, listenerAuth: ListenerAuth){
         viewModelScope.launch {
             authRepositorio.cadastro(nome, email, senha,rm, codigoturma, listenerAuth)
@@ -21,5 +22,13 @@ class AuthViewModel @Inject constructor(private val authRepositorio: AuthReposit
 
     fun login(email: String, senha: String, listenerAuth: ListenerAuth){
         authRepositorio.login(email,senha,listenerAuth)
+    }
+    /////////
+
+
+    //Viewmodel dos professores/administração
+    fun cpsCadastro(nome:String, email: String, senha: String, id:String, codigoEtec:String, listenerAuth: ListenerAuth){
+
+        cpsCadastro(nome, email, senha, id, codigoEtec, listenerAuth)
     }
 }
