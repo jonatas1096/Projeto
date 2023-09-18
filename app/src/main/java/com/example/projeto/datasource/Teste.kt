80bdcc8100f71fa184feaf18dc1e778123925301
AnahiMamani <narielanahi@gmail.com>
2023-09-18 16:44:52 -0300

    alteraçoes Perfil

---
+++ app/src/main/java/com/example/projeto/datasource/Teste.kt
@@ -0,0 +1,4 @@
+package com.example.projeto.datasource
+
+class Teste {
+}
--- app/src/main/java/com/example/projeto/views/Profile.kt
+++ app/src/main/java/com/example/projeto/views/Profile.kt
@@ -2,6 +2,7 @@ package com.example.projeto.views
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -46,22 +48,28 @@ fun Profile(navController: NavController){
         //Constraint do quadradao e do conteudo de dentro
             ConstraintLayout(modifier = Modifier.fillMaxSize())
             {
-                val (boxPrincipal, arrow)= createRefs()
+                val (boxPrincipal, arrow, juli) = createRefs()
 
                 //Arrow voltar (seta q volta)
-                Box(modifier = Modifier.constrainAs(arrow){
-                    top.linkTo(parent.top, margin = 10.dp)//toquei de 8dp para 10pdp (questao estetica apenas)
-                    start.linkTo(parent.start, margin = 18.dp)
-                }
+                Box(modifier = Modifier
+                    .constrainAs(arrow) {
+                        top.linkTo(
+                            parent.top,
+                            margin = 10.dp
+                        )//toquei de 8dp para 10pdp (questao estetica apenas)
+                        start.linkTo(parent.start, margin = 18.dp)
+                    }
                     .size(30.dp)
                     .clickable(onClick = {
                         navController.popBackStack()
                     })
                 ) {
-                    loadImage(path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/arrow.png",
+                    loadImage(
+                        path = "https://raw.githubusercontent.com/jonatas1096/Projeto/master/app/src/main/res/drawable/arrow.png",
                         contentDescription = "Icone para voltar de página",
                         contentScale = ContentScale.Crop,
-                        modifier = Modifier)
+                        modifier = Modifier
+                    )
                 }
                 //BOX DO QUADRADO 100%nofake
                 Box(modifier = Modifier
@@ -71,51 +79,27 @@ fun Profile(navController: NavController){
                         top.linkTo(parent.top, margin = 30.dp)
                         bottom.linkTo(parent.bottom)
                         end.linkTo(parent.end)
-                    }
+                    }//quero cetralizar toda a columm, como faze?
                     .width(360.dp)
                     .height(765.dp)
-                    .padding(horizontal = (70.dp))
-                )
-                {
-                    Column() {
+                    //.padding(horizontal = (100.dp))//aqui???
+                ) {
+                    Column(modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
                         Text(text = "AAAAAAAAAAA")
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Black, shape = RoundedCornerShape(20.dp))
+                                .width(100.dp)
+                                .height(100.dp)
+                                .padding(horizontal = (70.dp))
+                        )
+
                     }
                 }
             }
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        ConstraintLayout (modifier = Modifier.fillMaxSize()){
-
-            val (julio,ines) = createRefs() //criou as referencias
-
-            loadImage(path = "https://static.wikia.nocookie.net/cocorico/images/e/e3/Julio-careca.jpg/revision/latest?cb=20211011002720&path-prefix=pt-br",
-                contentDescription = "julio careca",
-                contentScale = ContentScale.None,
-                modifier = Modifier
-                    .size(120.dp)
-                    .constrainAs(julio) {
-                        bottom.linkTo(parent.bottom)
-                    }
-
-            )
-            loadImage(path = "https://rd1.com.br/wp-content/uploads/2016/06/In%C3%AAs-Brasil-32-810x442.jpg",
-                contentDescription = "ines",
-                contentScale = ContentScale.None,
-                modifier = Modifier
-                    .size(120.dp)
-                    .constrainAs(ines) {
-
-                        start.linkTo(julio.end) //primeiro membro: Ines // segundo membro: a referencia dela
-                        //em outras pálavras, o start da ines (a esquerda) vai começar à direita do julio.
-                        bottom.linkTo(parent.bottom, margin = 10.dp)
-                    }
-
-            )
-
-        }
-
+            }
 
     }
-
-}
+//aaaaaaaaaaaaaaaaaaaa
